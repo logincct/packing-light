4e20ccaf33ac20a4e696dada1bbcd24d51528643
Lucas Campelo <lucas_campelo3009@hotmail.com>
2020-03-06 15:31:12 -0300

    Clean na função de gerar o canvas.

--- Packing_Light/painel/pages/admin/canvas.php
+++ Packing_Light/painel/pages/admin/canvas.php
@@ -149,63 +149,59 @@
       <!-- /.row -->
 
 		<script>
-			//colors = ["#023ff7", "#f70202", "#f7f302", "#0af702"]
-			colors = ["#3c8dbc"]
+			color = "#3c8dbc";
 
 			var myCanvas = document.getElementById("Pallet");  //Select canvas element in the page
 			var ctx1 = myCanvas.getContext("2d");  //Built-in object
 
-			var num_obj = <?php echo $_SESSION['numeroItens']?>;
-			var x = 0;
+			var numeroItens = <?php echo $_SESSION['numeroItens']?>;
+      var larguraPallet = <?php echo $_SESSION['larguraPallet']; ?>;
+      var comprimentoPallet = <?php echo $_SESSION['comprimentoPallet']; ?>;
+			var larguraItem = <?php echo $_SESSION['larguraItem']; ?>;  //larguraItem é a largura do objeto
+			var comprimentoItem = <?php echo $_SESSION['comprimentoItem']; ?>;  //comprimentoItem é o comprimento do objeto
+
+      var x = 0;
 			var y = 0;
-			var lag_obj = 0;
-			var comp_obj = 0;
-			var cont = 1; //Contador para quantidade de caixas colocadas do pallet
+			
+      var cont = 1; //Contador para quantidade de caixas colocadas do pallet
 			var temp;
 			var contador = 1;
-
-				lag_obj = <?php echo $_SESSION['larguraItem']; ?>;  //lag_obj é a largura do objeto
-				comp_obj = <?php echo $_SESSION['comprimentoItem']; ?>;  //comp_obj é o comprimento do objeto
-
 
 			//Enquanto o número de caixas colocadas for menor que o número de objetos
 			while ( cont <= <?php echo $_SESSION['res']; ?> && cont <= <?php echo $_SESSION['numeroItens']; ?> ) {
 
 				//Desenhar as caixas
-				ctx1.fillStyle = colors[  Math.floor( (Math.random() * 1) ) ];  //Fill the color 
-				ctx1.fillRect(x, y, lag_obj , comp_obj);  //Create box
+				ctx1.fillStyle = color;  //Fill the color 
+				ctx1.fillRect(x, y, larguraItem , comprimentoItem);  //Create box
 				ctx1.beginPath();
 				ctx1.lineWidth = "1";
-				ctx1.rect(x, y, lag_obj , comp_obj);
+				ctx1.rect(x, y, larguraItem , comprimentoItem);
 				ctx1.stroke();
 
 				//Verifica se a caixa cabe e muda o X e o Y que não as coordenadas para as próximas caixas
-				if ( ((x + lag_obj) <= myCanvas.attributes[1].value) && (y + comp_obj <= myCanvas.attributes[2].value )) {
-					x += lag_obj;
+				if ( ((x + larguraItem) <= larguraPallet) && (y + comprimentoItem <= comprimentoPallet )) {
+					x += larguraItem;
 				}
 
-				if ( (x + lag_obj) > myCanvas.attributes[1].value && (y + comp_obj) <= myCanvas.attributes[2].value ) {
+				if ( (x + larguraItem) > larguraPallet && (y + comprimentoItem) <= comprimentoPallet ) {
 					x = 0;
-					y += comp_obj;				
+					y += comprimentoItem;				
 				}
-				if( (y + comp_obj) > myCanvas.attributes[2].value ){
+				if( (y + comprimentoItem) > comprimentoPallet ){
 					break;
 				}
 
 
 				cont = cont + 1;
-				num_obj -= 1;
+				numeroItens -= 1;
 
 			}
 
-			//document.write(cont);
 			if(<?php echo $_SESSION['espaçoCaixaLargura'];?> == 1){
 
 				x = <?php echo $_SESSION['larguraPallet'] - $_SESSION['larguraRestantePallet']; ?>;
 				y = 0;
-				//document.write("AA");
 			}else if(<?php echo $_SESSION['espaçoCaixaComprimento'];?> == 1){
-				//document.write("AA");
 				x = 0;
 				y = <?php echo $_SESSION['comprimentoPallet'] - $_SESSION['comprimentoRestantePallet']; ?>;
 
@@ -213,41 +209,35 @@
 
 			}
 
-			temp = comp_obj;
-			comp_obj = lag_obj;
-			lag_obj = temp;
+			temp = comprimentoItem;
+			comprimentoItem = larguraItem;
+			larguraItem = temp;
 			
 			
-			//document.write(cont);
 			while (<?php echo $_SESSION['espaçoRestante']; ?> ==1 && ((cont+1) <= (contador + <?php echo $_SESSION['res']; ?>) && ((cont+1) <= <?php echo $_SESSION['numeroItens']; ?>))) {
-				//while(){
-				//document.write(contador);
 				//Desenhar as caixas
-				ctx1.fillStyle = colors[  Math.floor((Math.random() * 3) + 1) ];  //Fill the color 
-				ctx1.fillRect(x, y, lag_obj , comp_obj);  //Create box
+				ctx1.fillStyle = color;  //Fill the color 
+				ctx1.fillRect(x, y, larguraItem , comprimentoItem);  //Create box
 				ctx1.beginPath();
 				ctx1.lineWidth = "1";
-				ctx1.rect(x, y, lag_obj , comp_obj);
+				ctx1.rect(x, y, larguraItem , comprimentoItem);
 				ctx1.stroke();
 
 				//Verifica se a caixa cabe e muda o X e o Y que não as coordenadas para as próximas caixas
-				if ( ((x + lag_obj) <= myCanvas.attributes[1].value) && (y + comp_obj <= myCanvas.attributes[2].value )) {
-					x += lag_obj;
+				if ( ((x + larguraItem) <= larguraPallet) && (y + comprimentoItem <= comprimentoPallet )) {
+					x += larguraItem;
 				}
 
-				if ( (x + lag_obj) > myCanvas.attributes[1].value && (y + comp_obj) <= myCanvas.attributes[2].value ) {
+				if ( (x + larguraItem) > larguraPallet && (y + comprimentoItem) <= comprimentoPallet ) {
 					x = <?php echo $_SESSION['larguraPallet'] - $_SESSION['larguraRestantePallet'] ?>;
-					y += comp_obj;				
+					y += comprimentoItem;				
 				}
-				if( (y + comp_obj) > myCanvas.attributes[2].value ){
+				if( (y + comprimentoItem) > comprimentoPallet ){
 					break;
 				}
 				cont = cont + 1;
 				contador = contador + 1;
-
-			//}
-			//cont = cont + 1;
-		}
+		  }
 		
 		</script>
 
