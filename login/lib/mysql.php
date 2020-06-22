6d3fc1d8eff7fc3aea7b36ba4b7063c164e2c2e6
jamillepp <jamillep94@gmail.com>
2020-06-22 18:33:20 -0300

    Trying to connect database.10

--- login/lib/mysql.php
+++ login/lib/mysql.php
@@ -16,7 +16,7 @@
 		
 		function __construct(){
 			
-			$url = parse_url(getenv("mysql://b32a123ad227fe:5426c7da@us-cdbr-east-05.cleardb.net/heroku_0762b6896b82648?reconnect=true"));
+			$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 			$host = $url["host"];
 			$user = $url["user"];
