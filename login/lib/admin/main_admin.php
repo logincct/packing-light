954a99677e04944cd7b33af72350bce9ef1d4357
jamillepp <jamillep94@gmail.com>
2020-07-06 18:10:20 -0300

    Revert "Teste erro"
    This reverts commit 215208a5b4c0aa5937a4fc7b8ce5101dad2f17ab.

--- login/lib/admin/main_admin.php
+++ login/lib/admin/main_admin.php
@@ -1,38 +1,35 @@
 <?php
     session_start();
+    $sistema = "";
+    $nivel = "";
+    $_SESSION["check"] = 1;
+    if(isset($_POST["submit"])){
 
-    echo "<script>javascript:window.location.replace('../../../rotalight/index.php');</script>";
-    
-    // $sistema = "";
-    // $nivel = "";
-    // $_SESSION["check"] = 1;
-    // if(isset($_POST["submit"])){
+        if(isset($_POST["sistema"])){
+        $sistema = $_POST["sistema"];
+        }
 
-    //     if(isset($_POST["sistema"])){
-    //     $sistema = $_POST["sistema"];
-    //     }
+        $session = $_SESSION["admin"];
 
-    //     $session = $_SESSION["admin"];
+        if (isset($session)) {
 
-    //     if (isset($session)) {
+            echo "<script>javascript:window.location.replace('../../../rotalight/index.php');</script>";
 
-    //         echo "<script>javascript:window.location.replace('../../../rotalight/index.php');</script>";
+        }
+    }
+    if(isset($_POST["submit_pack"])){
 
-    //     }
-    // }
-    // if(isset($_POST["submit_pack"])){
+        if(isset($_POST["sistema"])){
+            $sistema = $_POST["sistema"];
+        }
 
-    //     if(isset($_POST["sistema"])){
-    //         $sistema = $_POST["sistema"];
-    //     }
+        $session = $_SESSION["admin"];
 
-    //     $session = $_SESSION["admin"];
+        if (isset($session)) {
 
-    //     if (isset($session)) {
+            echo "<script>javascript:window.location.replace('../../../Packing_Light/index.php');</script>";
 
-    //         echo "<script>javascript:window.location.replace('../../../Packing_Light/index.php');</script>";
-
-    //     }
-    // }
+        }
+    }
 
 ?>
