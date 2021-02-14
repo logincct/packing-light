f83c660dff2e40bd91db198190f4cb413c26a44f
Rebeca Teófilo Siqueira <login.cct@uece.br>
2021-02-14 16:17:44 -0300

    Teste 23 para função shell_exec

--- login/lib/index.php
+++ login/lib/index.php
@@ -19,7 +19,7 @@
         $senha = md5($senha_md5);
     }    
         
-    shell_exec("admin/main_admin.php", $retorno);
+    exec("admin/main_admin.php", $retorno);
     
     /*if($email != ""){
         echo "<script>javascript:window.alert('{$email}');</script>";
