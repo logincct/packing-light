8a67e15df72fb49b04d42ae749e304853bd1b75d
Rebeca Teófilo Siqueira <login.cct@uece.br>
2021-05-01 22:47:39 -0300

    Atualização de quebra da variavel dos arquivos

--- Packing_Light/lib/buscaObjeto3D.php
+++ Packing_Light/lib/buscaObjeto3D.php
@@ -1,76 +1,49 @@
 <?php
 	session_start();
-	// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-	// $host = $url["host"];
-	// $user = $url["user"];
-	// $pass = $url["pass"];
-	// $db = substr($url["path"], 1);
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
-	$db = "login";
-
-	$connect = mysqli_connect($host, $user, $pass, $db);
-  	$listar = mysqli_query($connect, "SELECT codigo,nome,largura,comprimento,altura,peso FROM busca_objetos");
-
   	
   	if (isset($_POST["adiciona"])) {
-  		$_SESSION['nome'] = $_POST['nome_obj'];
-		$_SESSION['l_palt']  = $_POST['l_palt'];
-		$_SESSION['c_palt'] = $_POST['c_palt'];
-		$_SESSION['h_palt'] = $_POST['h_palt'];
-		$_SESSION['carga'] = $_POST['carga'];
+  		$nome = $_POST['nome_obj'];
+		$l_palt  = $_POST['l_palt'];
+		$c_palt = $_POST['c_palt'];
+		$h_palt = $_POST['h_palt'];
+		$carga = $_POST['carga'];
 
 
-		while ($row = mysqli_fetch_assoc($listar)){
-			if($row['nome'] == $_SESSION['nome']){
-				$_SESSION['cod_obj'] = $row['codigo'];
-				$_SESSION['nome_obj'] = $row['nome'];
-				$_SESSION['larg_obj'] = $row['largura'];
-				$_SESSION['comp_obj'] = $row['comprimento'];
-				$_SESSION['alt_obj'] = $row['altura'];
-				$_SESSION['peso_obj'] = $row['peso'];
-				//array_push($lista3, $_SESSION['cod_obj'], $_SESSION['nome_obj'], $_SESSION['larg_obj'], $_SESSION['comp_obj'], $_SESSION['alt_obj'], $_SESSION['peso_obj']);
-			//$lista2 .= $lista3 . "";
-			}
-		}
-
-
-		$arquivo = 'texto.json';
-		$conteudo = "";
+		$arquivo = 'pallet.json';
+		$texto = "";
 		$fp = fopen($arquivo, 'w');
-
-		fwrite($fp, $conteudo);
+		fwrite($fp, $texto);
 		fclose($fp);
 
-		
-			//$_SESSION["lista1"] = array('Pallets' => array("l_palt" => $_SESSION["l_palt"]), array("c_palt" => $_SESSION["c_palt"]), array("h_palt" => $_SESSION["h_palt"]), array("carga" => $_SESSION["carga"]));
-			$_SESSION["lista1"][] =  array('nome'=>$_SESSION["l_palt"], 'c_palt'=>$_SESSION["c_palt"]);
-			//session_destroy();
-			//echo $_SESSION["lista1"]['nome'];
-		
-			$conteudo1 = json_encode($_SESSION["lista1"]);
-			echo $conteudo1;
+		$arquivo1 = 'produto.json';
+		$texto2 = "";
+		$fp1 = fopen($arquivo1, 'w');
+		fwrite($fp1, $texto2);
+		fclose($fp1);
 
-			$fp1 = fopen($arquivo, 'a+');
+			$_SESSION["lista1"] = array('l_palt'=>$l_palt, 'c_palt'=>$c_palt, 'h_palt'=>$h_palt, 'carga'=>$carga);
+			$_SESSION["lista2"][] = $nome;
+			session_destroy();
+			$conteudo = json_encode($_SESSION["lista1"]);
+			$conteudo1 = json_encode($_SESSION["lista2"]);
 
-			fwrite($fp1, print_r($conteudo1, true));
-			//fwrite($fp1, print_r($conteudo2, true));
-			fclose($fp1);
-			$string = file_get_contents("texto.json");
+			$fp3 = fopen($arquivo, 'a+');
+			fwrite($fp3, print_r($conteudo, true));
+			fclose($fp3);
 
+			$fp4 = fopen($arquivo1, 'a+');
+			fwrite($fp4, print_r($conteudo1, true));
+			fclose($fp4);
 
-		//echo "<script>javascript:window.location.replace('../pages/buscar_objeto_3d.php');</script>";
-
+		echo "<script>javascript:window.location.replace('../pages/buscar_objeto_3d.php');</script>";
 
   	}
   	
 	if (isset($_POST['calculo'])) {	
 		$return = array();
-		$arr = "";
   		$comando = "go run ../../bp3d-master/example/testando.go";
-		$saida = exec($comando, $return, $arr);
+		$saida = exec($comando, $return);
 		print_r($saida);
+
 	}
 
--- bp3d-master/example/testando.go
+++ bp3d-master/example/testando.go
@@ -1,40 +1,85 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 
+	"fmt"
+	"encoding/json"
 	"os"
+	"io/ioutil"
+	"bufio"
+	"strings"
 )
-func lerTexto(caminhoDoArquivo string) ([]string) {
-	// Abre o arquivo
-	arquivo, err := os.Open(caminhoDoArquivo)
-	// Caso tenha encontrado algum erro ao tentar abrir o arquivo retorne o erro encontrado
+type Pallet struct {
+    Pallet []Pallets `json:Pallets"`
+}
+type Pallets struct {
+
+    L_palt   string 
+    C_palt   string 
+    H_palt    string    
+    Carga string 
+}
+
+type Product struct {
+	Nome string `json:"nome"`
+}
+
+func main() {
+
+	arquivo, err := os.Open("../../Packing_Light/lib/pallet.json")
+	arquivos, err := os.Open("../../Packing_Light/lib/produto.txt")
+
 	if err != nil {
-		
+		fmt.Println(err)
+		fmt.Println("erro")
 	}
-	// Garante que o arquivo sera fechado apos o uso
+
+	fmt.Println("Successfully Opened json")
+
 	defer arquivo.Close()
 
-	// Cria um scanner que le cada linha do arquivo
+	var pallets Pallets
+	//var product Product
+	byteValue, _ := ioutil.ReadAll(arquivo)
+
+
+	defer arquivos.Close()
+
+
 	var linhas []string
-	scanner := bufio.NewScanner(arquivo)
+	scanner := bufio.NewScanner(arquivos)
 	for scanner.Scan() {
 		linhas = append(linhas, scanner.Text())
 	}
 
-	// Retorna as linhas lidas e um erro se ocorrer algum erro no scanner
-	return linhas
-}
 
-func main() {
-	var conteudo []string
-
-	conteudo = nil
-	conteudo = lerTexto("../../Packing_Light/lib/texto.json")
+	s := strings.Join(linhas, "")
 
 
-	for indice, linha := range conteudo {
-		fmt.Println(indice, linha[0])
+    replacer := strings.NewReplacer("[[", "", "]]", "", "]", "", "[", "","\"", "")
+	s = replacer.Replace(s)
+	words := strings.Fields(s)
+	split := strings.Split(words[0], ",")
+
+
+	//fmt.Println(split)
+
+	for i, splits := range split {
+	  	inc, vari := i, splits
+
+	  	fmt.Println(vari)
+	  	fmt.Println(inc)
 	}
-}
+
+	//fmt.Println(echo)
+	//byteValue1, _ := ioutil.ReadAll(arquivo1)
+	
+	erro := json.Unmarshal(byteValue, &pallets)
+
+
+	if erro != nil {
+	    fmt.Println("unmarshalling error ", erro)
+	    return
+	}
+
+	}
